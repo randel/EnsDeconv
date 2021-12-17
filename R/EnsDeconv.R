42521f26029c8efe1adee47ea14077b7d582fe3c
ManqiCai <manqi.cai.mk@gmail.com>
2021-12-17 00:13:24 -0500

    Update pkgdown and testdata

--- R/EnsDeconv.R
+++ R/EnsDeconv.R
@@ -1,44 +1,37 @@
 #' This function is a wrapper for EnsDeconv
 #'
 #' @param count_bulk Bulk gene expression.
-#' (Required)  Two-dimensional numeric. Must be in gene x sample format. Must implemented \code{as.matrix}
+#' 
+#' (Required)  Two-dimensional numeric. Must be in gene x sample format. Must implemented \code{as.matrix}.
 #' (Optional) In original scale.
-#' @param meta_bulk Meta data for bulk gene expression.
-#' (Optional) Dataframe, much include character variable SamplesName.
-#' (Required) If reference data is single cell rna seq.
 #' @param ref_list List of of list. Must implement as.list.
-#'
 #'
 #' (Required) The i-th element of the top-level list is a list of \code{ref_matrix}, \code{meta_ref}. Names of the top level list should be vector of
 #' \code{data_name}, namely : bulk-reference.
 #'
 #' \itemize{
-#' \item{'ref_matrix'}{ Reference matrix.}
-#' \item{'meta_ref'}{ Meta data for reference matrix..}
-#' \item{'data_name'}{Data description. Character in format "Bulk data name_reference data name"}
+#' \item{'ref_matrix'}{ Reference matrix, rows are genes, columns are samples.}
+#' \item{'meta_ref'}{ Meta data for reference data. Must include two variables: "SamplesName" (column names of ref_matrix), "deconv_clust" (deconvolution cluster, eg. cell types).}
+#' \item{'data_name'}{Data description. Character in format "bulk data name_reference data name"}
 #' }
 #'
-#' @param customed_markers Self-defined markers.
-#' (Optional) List of one-dimensional string Names of the list should match the deconv_clust.
-#' @param markers_range Specific for markerpen.
-#' (Optional)
-#' @param true_frac True cell type proportions for bulk gene expresseeion.
-#' (Optional) Two-dimensional numeric. Must be in samples by celltype.
-#' @param params Parameters dataframe for ensemble learning, more details could refer to \code{get_params}.
+#' @param customed_markers (Optional) Self-defined markers.
+#' 
+#' @param true_frac (Optional) True cell type proportions for bulk gene expression
+#' Two-dimensional numeric. Must be in samples by cell type.
+#' @param params Parameters Dataframe for ensemble learning, more details could refer to \code{get_params}.
 #' @param outpath (Optional) Path to save output.
-#' @param data_name Data description.
-#' (Optional) Only input when you want default params. Character in format "Bulk data name-reference data name"
-#' @param parallel_comp Logical.
+#' @param parallel_comp Logical. Use parallel computing or not. Default: FALSE.
 #' @param  ncore 	The number of cores to use for parallel execution.
 #' @param rm.duplicated Logical. Remove duplicated genes after maker gene selection. Default: FALSE.
 #' @param mrkpen Logical. Apply markerpen on marker gene list. Default: FALSE.
-#' 
+#' @param markers_range (Optional) Specific for markerpen.
 #' @importFrom matrixcalc frobenius.norm
-#' @return A list containing the output of the EnsDeconv algorithm and output from each scenarios
+#' @return A list containing the output of the EnsDeconv algorithm (EnsDeconv) and a list output from each scenario (allgene_res). 
 #' @export
 
-EnsDeconv <- function(count_bulk,meta_bulk = NULL,ref_list,customed_markers = NULL,markers_range = NULL,true_frac = NULL,params = NULL,
-                        outpath = NULL,parallel_comp = FALSE,ncore,rm.duplicated =FALSE,mrkpen = FALSE,dmeths = NULL,trueMet){
+EnsDeconv <- function(count_bulk,ref_list,customed_markers = NULL,true_frac = NULL,params = NULL,
+                        outpath = NULL,parallel_comp = FALSE,ncore,rm.duplicated =FALSE,mrkpen = FALSE,markers_range = NULL,dmeths = NULL){
 
 
    allgene_res = gen_all_res_list(count_bulk = as.matrix(count_bulk), meta_bulk = meta_bulk, ref_list = ref_list, true_frac =true_frac, outpath =outpath, ncore =ncore, parallel_comp = parallel_comp, params = params,dmeths = dmeths)
---
+++ R/testdata.R
@@ -0,0 +1,11 @@
+#' testdata.
+#'
+#' A dataset containing a sample bulk dataset and sample reference data.
+#'
+#' @format A list of count_bulk and ref_list:
+#' \describe{
+#'   \item{count_bulk}{sample bulk data, where rows are genes and columns are samples}
+#'   \item{ref_list}{list of references}
+#'   ...
+#' }
+"testdata"
